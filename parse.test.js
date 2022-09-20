3d51335d3c1c0a62923acc9eeaa9b8f92bd76858
이진우 <ljw041101@gmail.com>
2022-09-20 10:51:42 +0900

    RED : '2022' 입력 시 2022가 아닌 02 출력

--- parse.test.js
+++ parse.test.js
@@ -8,4 +8,8 @@ describe("parse", () => {
   it("문자열 true를 입력하면 Boolean 값 true를 출력한다.", () => {
     expect(parse("true")).toBe(true);
   });
+
+  it("문자열 2022를 입력하면 숫자 2022를 출력한다.", () => {
+    expect(parse("2022")).toBe(2022);
+  });
 });
