1ec5e7414e9ce10ee0e8b82d470fb4e06547664c
이진우 <ljw041101@gmail.com>
2022-09-20 10:30:46 +0900

    RED : 'true' 입력 시 Boolean 값 true 가 아닌 'ru' 출력

--- parse.test.js
+++ parse.test.js
@@ -1,7 +1,11 @@
 const parse = require("./parse");
 
 describe("parse", () => {
-  it("문자열 jest를 넣으면 문자열 jest가 나온다", () => {
-    expect(parse('"jest"')).toEqual("jest");
+  it("문자열 'jest'를 넣으면 문자열 jest를 출력한다.", () => {
+    expect(parse('"jest"')).toBe("jest");
+  });
+
+  it("문자열 true를 입력하면 Boolean 값 true를 출력한다.", () => {
+    expect(parse("true")).toBe(true);
   });
 });
